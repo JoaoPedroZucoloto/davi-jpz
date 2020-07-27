415dbec59b88c5dcd9fe8bf5a4d56aca17c1bdac
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-27 13:57:21 -0300

    18

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def webhook_handle():
     if challenge != '*' and verify_token == 'chupacabra':
         return challenge
 
-    data = request.data.decode('utf-8')
-    return jsonify(data.get_json())
+    data = request.data
+    return jsonify(data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
