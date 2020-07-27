63e8d8ebc7de112d7295f080ff2d5241bdefa800
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-27 12:38:13 -0300

    4

--- app.py
+++ app.py
@@ -3,26 +3,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def nao_entre_em_panico():
-    ar = 'Lente Azul'
+@application.route('/webhook', methods=['GET', 'POST'])
+def webhook():    
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'chupacabra':
         return challenge
-
-    return request
-
-@app.route("/webhook", methods=['GET','POST'])
-def webhook_handle():
-    ar = 'Lente Azul'
-    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
-    verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
-    if challenge != '*' and verify_token == 'chupacabra':
-        return challenge
-
-    output = request.get_json()
-    return output
+       
+    data = request.data.decode('utf-8')
+        
+    return 'Oi :)'
 
 
 if __name__ == "__main__":
