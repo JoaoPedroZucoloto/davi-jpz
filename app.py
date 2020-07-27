11aac317b5ace6d39940d296dfc0cb49f73bb790
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-27 15:08:55 -0300

    24

--- app.py
+++ app.py
@@ -2,23 +2,25 @@ import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def nao_entre_em_panico():
-    with open('caminho-completo/teste.txt', 'a') as file:
-            file.write(request.data.decode('utf-8'))
-            file.read()
+WP_API_URL   = 'https://graph.facebook.com/v2.6/me/messages?access_token=TEU-TOKEN-AQUI'
 
 @app.route("/webhook", methods=['GET'])
 def webhook_handle():
-    ar = 'Lente Azul'
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
-    if challenge == '*' and verify_token == 'chupacabra':
+    if challenge != '*' and verify_token == 'chupacabra':
         return challenge
 
-    data = request.data.decode('utf-8')
+    #data = request.data.decode('utf-8')
+    data = json.loads(request.data.decode('utf-8'))
     return jsonify(data)
+
+
+def send_message(recipient_id, text): 
+    response = requests.post(WP_API_URL, headers=WP_HEADER, data=json.dumps({
+        'recipient' : {'id'   : recipient_id},
+        'message'   : {'text' : text}
+    }))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
