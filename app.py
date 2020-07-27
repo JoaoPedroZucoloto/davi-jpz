000db735dfa6244dc98e2436cd5e9b25f9bb3f39
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-27 12:48:33 -0300

    7

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():    
+@app.route('/', methods=['GET', 'POST'])
+def nao_entre_em_panico():
+    with open('caminho-completo/teste.txt', 'a') as file:
+            file.write(request.data.decode('utf-8'))
+            file.read()
+
+@app.route("/webhook", methods=['GET','POST'])
+def webhook_handle():
+    ar = 'Lente Azul'
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'chupacabra':
         return challenge
-       
-    data = request.data.decode('utf-8')
-        
-    return 'Oi :)'
 
+    output = request.get_json()
+    return output
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
